836eb7abbb399edb83d232d312a85b72037bf01a
Wanderson Andrade <wanderson.andrade@gmail.com>
2025-04-27 01:09:32 -0300

    exercicio revisto

--- exercicio_4.py
+++ exercicio_4.py
@@ -41,18 +41,18 @@ lista_de_organismos = [[50, 50, 50], [125, 99, 12], [19, 91, 42], [40, 189, 0], 
 
 # Fazer a partir daqui
 
-posicao_organismo = 1
+posicao_organismo = 0
 maior_organismo = 0
 maior_media = 0
 for organismo in lista_de_organismos:
   total_leitura_organismo = 0
   media_leitura_organismo = 0
-  posicao_organismo +=1
   for leitura in organismo:
     total_leitura_organismo += leitura
   media_leitura_organismo = total_leitura_organismo/len(organismo)
   if media_leitura_organismo > maior_media:
     maior_media = media_leitura_organismo
     maior_organismo = posicao_organismo
+  posicao_organismo +=1
   #print(f'Para o organismo: {posicao_organismo} o total leitura é: {total_leitura_organismo} e Media é: {media_leitura_organismo}')
 print(f'O organismo com maior média é o da posição {maior_organismo} da lista.')
