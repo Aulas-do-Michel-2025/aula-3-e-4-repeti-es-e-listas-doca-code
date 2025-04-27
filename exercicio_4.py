f2659611229901270e0217e952b472fc9320998d
Wanderson Andrade <wanderson.andrade@gmail.com>
2025-04-27 01:01:13 -0300

    exercicio revisto

--- exercicio_4.py
+++ exercicio_4.py
@@ -40,7 +40,6 @@ O cálculo de média já foi feito em sala e pode ser usado de exemplo.
 lista_de_organismos = [[50, 50, 50], [125, 99, 12], [19, 91, 42], [40, 189, 0], [1, 0, 0], [100, 100, 70], [99, 12, 12]]
 
 # Fazer a partir daqui
-lista_de_organismos = [[50, 50, 50], [125, 99, 12], [19, 91, 42], [40, 189, 0], [1, 0, 0], [100, 100, 70], [99, 12, 12]]
 
 posicao_organismo = 0
 maior_organismo = 0
